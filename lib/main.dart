2a37e1d9661ff8a6a1961c63bbb4de654e525d41
navinpatel <recs.cse1628@gmail.com>
2025-12-25 19:19:46 +0530

    create build release

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:pro/screen/login/login_screen.dart';
-import 'package:pro/widget/login_button_widget.dart';
-import 'package:pro/widget/text_field_widget.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
--- lib/screen/home/home_screen.dart
+++ lib/screen/home/home_screen.dart
@@ -55,7 +55,7 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
       body: SafeArea(child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 18.0),
-        child: Text("Hi"),
+        child: Text("Navin"),
       )),);
 
   }
